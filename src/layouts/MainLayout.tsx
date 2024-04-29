7c0e1fcc30bc4dd367a9e83e56f4dfa066cfae78
Tetiankaa <tanyakorsun0208@gmail.com>
2024-04-29 20:59:00 +0200

    1. Implemented hover effect for each product. 2. Implemented background color based on high of the content

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -6,7 +6,7 @@ const MainLayout = () => {
     return (
         <div>
             <Header/>
-            <div className={"container-lg shadow p-3 mb-5"} style={{background:"rgba(208, 234, 241, 0.8)", height: '100vh'}}><Outlet/></div>
+            <div className={"container-lg shadow p-3 mb-5"} style={{background:"rgba(208, 234, 241, 0.8)"}}><Outlet/></div>
         </div>
     );
 };
